ee4a6056c7ae356f3e212e084fdd706fe305b281
2382032 <2382032@unai.edu>
2024-09-17 21:28:04 +0700

    handson3

---
+++ soal3/DoWhile3.java
@@ -0,0 +1,19 @@
+package soal3;
+
+public class DoWhile3 {
+    public static void main(String[] args) {
+        int counter = 1;
+
+        do{
+            if (counter > 15){
+                break;
+            }
+
+            if (counter % 3 != 0){
+                System.out.println(counter + " ");
+            }
+
+            counter++;
+        }while (counter <= 20);
+    }
+}
